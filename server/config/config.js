b5e0719642213b0b42ea993dd23ce7684e3db21e
Vitor Orsini <vitorluigiorsini@gmail.com>
2023-06-30 15:19:28 -0300

    config datava base connection

--- server/config/config.js
+++ server/config/config.js
@@ -2,29 +2,29 @@ require('dotenv').config()
 
 module.exports = {
   development: {
-    username: process.env.POSTGRESQL_USERNAME,
-    password: process.env.POSTGRESQL_PASSWORD,
-    database: process.env.POSTGRESQL_DATABASE,
-    host: process.env.POSTGRESQL_HOST,
-    port: process.env.POSTGRESQL_PORT,
-    dialect: process.env.POSTGRESQL_DIALECT,
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    dialect: process.env.PGDIALECT,
+    port: process.env.PGPORT,
     logging: true
   },
   test: {
-    username: process.env.POSTGRESQL_USERNAME,
-    password: process.env.POSTGRESQL_PASSWORD,
-    database: process.env.POSTGRESQL_DATABASE,
-    host: process.env.POSTGRESQL_HOST,
-    port: process.env.POSTGRESQL_PORT,
-    dialect: process.env.POSTGRESQL_DIALECT,
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    dialect: process.env.PGDIALECT,
+    port: process.env.PGPORT,
     logging: true
   },
   production: {
-    username: process.env.POSTGRESQL_USERNAME,
-    password: process.env.POSTGRESQL_PASSWORD,
-    database: process.env.POSTGRESQL_DATABASE,
-    host: process.env.POSTGRESQL_HOST,
-    port: process.env.POSTGRESQL_PORT,
-    dialect: process.env.POSTGRESQL_DIALECT
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    dialect: process.env.PGDIALECT,
+    port: process.env.PGPORT
   }
 }
--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(process.env.POSTGRES_URL)
+const sequelize = new Sequelize(process.env.DATABASE_URL)
 
 module.exports = sequelize
